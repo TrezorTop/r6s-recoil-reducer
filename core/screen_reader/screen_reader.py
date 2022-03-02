4fce124e77225912eb3dc9915fa2a63b1dd9c8b4
Andrey Seluykov <trezor6@mail.ru>
2022-03-02 21:06:28 +0300

    update

--- core/screen_reader/screen_reader.py
+++ core/screen_reader/screen_reader.py
@@ -21,10 +21,14 @@ def determine_profile():
 
     for key in [element for element in app_data.get_profile_list() if
                 element not in {'profile_1', 'profile_2', 'profile_3', 'default'}]:
-        res = imagesearcharea('./settings/icons_to_search/' + key + '.png', x0, y0, x1, y1, 0.9, region)
-        if res[0] != -1:
-            app_state.set_forces(get_profile(key))
-            return
+        try:
+            res = imagesearcharea('./settings/icons_to_search/' + key + '.png', x0, y0, x1, y1, 0.9, region)
+
+            if res[0] != -1:
+                app_state.set_forces(get_profile(key))
+                return
+        except FileNotFoundError:
+            print('[WARNING]:', key, 'image not found')
 
     print("nothing found, setting default")
     app_state.set_forces(get_profile("default"))
